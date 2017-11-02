d5fad62c19731b7913334986e959e394519e5735
Phan Khanh <khanhpq@Phongs-Mac-mini.local>
2017-11-02 10:59:40 +0700

    remove unused code

--- app/src/main/java/ht/pq/khanh/notification/ReminderNotificationService.kt
+++ app/src/main/java/ht/pq/khanh/notification/ReminderNotificationService.kt
@@ -6,8 +6,6 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import com.pawegio.kandroid.d
 import ht.pq.khanh.multitask.R
 import ht.pq.khanh.task.reminder.ReminderActivity
 import ht.pq.khanh.util.Common
