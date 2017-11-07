6d74538c54d1cc27459de9e51c9e8f7d30f52e2a
Phan Khanh <khanhpq@Phongs-Mac-mini.local>
2017-11-07 17:31:48 +0700

    disable room

--- app/src/main/java/ht/pq/khanh/TaskApplication.kt
+++ app/src/main/java/ht/pq/khanh/TaskApplication.kt
@@ -1,20 +1,18 @@
 package ht.pq.khanh
 
 import android.app.Application
-import android.arch.persistence.room.Room
 import android.content.Context
 import com.squareup.leakcanary.LeakCanary
 import com.squareup.leakcanary.RefWatcher
-import ht.pq.khanh.model.TaskDatabase
 
 /**
  * Created by khanhpq on 10/3/17.
  */
 class TaskApplication : Application() {
     private lateinit var refWatcher: RefWatcher
-    companion object {
-        var taskDb : TaskDatabase? = null
-    }
+//    companion object {
+//        var taskDb : TaskDatabase? = null
+//    }
 
     fun getRefWatcher(context: Context): RefWatcher {
         val application = context.applicationContext as TaskApplication
@@ -24,6 +22,6 @@ class TaskApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         refWatcher = LeakCanary.install(this)
-        taskDb = Room.databaseBuilder(this, TaskDatabase::class.java, "task").build()
+//        taskDb = Room.databaseBuilder(this, TaskDatabase::class.java, "task").build()
     }
 }
