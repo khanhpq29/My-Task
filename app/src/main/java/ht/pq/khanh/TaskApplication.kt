f4d15f5f83db3ff0ba574d57661bcdab303b3dff
Phan Khanh <khanhpq@Phongs-Mac-mini.local>
2017-11-10 18:36:31 +0700

    remove unused code and wrong

--- app/src/main/java/ht/pq/khanh/TaskApplication.kt
+++ app/src/main/java/ht/pq/khanh/TaskApplication.kt
@@ -1,20 +1,15 @@
 package ht.pq.khanh
 
 import android.app.Application
-import android.arch.persistence.room.Room
 import android.content.Context
 import com.squareup.leakcanary.LeakCanary
 import com.squareup.leakcanary.RefWatcher
-import ht.pq.khanh.model.TaskDatabase
 
 /**
  * Created by khanhpq on 10/3/17.
  */
 class TaskApplication : Application() {
     private lateinit var refWatcher: RefWatcher
-    companion object {
-        lateinit var db : TaskDatabase
-    }
     fun getRefWatcher(context: Context): RefWatcher {
         val application = context.applicationContext as TaskApplication
         return application.refWatcher
@@ -23,6 +18,5 @@ class TaskApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         refWatcher = LeakCanary.install(this)
-        TaskApplication.db = Room.databaseBuilder(this, TaskDatabase::class.java, "note.db").build()
     }
 }
--- app/src/main/java/ht/pq/khanh/multitask/Example.kt
+++ app/src/main/java/ht/pq/khanh/multitask/Example.kt
@@ -10,16 +10,8 @@ import android.widget.EditText
 import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
-import ht.pq.khanh.TaskApplication
 import ht.pq.khanh.extension.inflateLayout
-import ht.pq.khanh.extension.showToast
-import ht.pq.khanh.model.Note
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.functions.BiConsumer
-import io.reactivex.schedulers.Schedulers
-import org.jetbrains.anko.sdk25.coroutines.onClick
 
 /**
  * Created by khanhpq on 11/8/17.
@@ -30,7 +22,6 @@ class Example : Fragment() {
     lateinit var hours: EditText
     @BindView(R.id.btnInsert)
     lateinit var btnInsert: Button
-    private val disposal: CompositeDisposable by lazy { CompositeDisposable() }
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = container!!.inflateLayout(R.layout.layout_example)
         ButterKnife.bind(this, view)
@@ -43,27 +34,16 @@ class Example : Fragment() {
 
     @OnClick(R.id.btnGetAll)
     fun getAll() {
-        disposal.add(
-                Single.fromCallable {TaskApplication.db.noteDao().getAllNote()}.subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe({notes -> activity.showToast("${notes.size}") },
-                                { error -> error.printStackTrace() })
-        )
+
     }
 
     @OnClick(R.id.btnInsert)
     fun insert() {
-        val content = hours.text.toString()
-        val note = Note(ht.pq.khanh.extension.getId(), content)
-        disposal.add(Single.fromCallable {
-            TaskApplication.db.noteDao().insertNote(note)
-        }.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread()).subscribe())
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        disposal.clear()
     }
 }
 
