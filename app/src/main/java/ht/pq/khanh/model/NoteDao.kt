1b7f90295580b9e6df52d687d9ce96e16e4f28bc
Phan Khanh <khanhpq@Phongs-Mac-mini.local>
2017-11-10 13:43:09 +0700

    grade

--- app/src/main/java/ht/pq/khanh/model/NoteDao.kt
+++ app/src/main/java/ht/pq/khanh/model/NoteDao.kt
@@ -4,9 +4,6 @@ import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.Query
 
-import io.reactivex.Flowable
-import retrofit2.http.Query
-
 /**
  * Created by khanhpq on 11/10/17.
  */
--- app/src/main/java/ht/pq/khanh/model/TaskDatabase.kt
+++ app/src/main/java/ht/pq/khanh/model/TaskDatabase.kt
@@ -2,17 +2,13 @@ package ht.pq.khanh.model
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
-import ht.pq.khanh.model.alarm.Alarm
-import ht.pq.khanh.model.alarm.AlarmDao
-import ht.pq.khanh.model.reminder.Reminder
-import ht.pq.khanh.model.reminder.ReminderDao
 
 /**
  * Created by khanhpq on 11/10/17.
  */
-@Database(entities = arrayOf(Alarm::class, Reminder::class, Note::class), version = 1)
+@Database(entities = arrayOf(/*Alarm::class, Reminder::class, */Note::class), version = 1)
 abstract class TaskDatabase : RoomDatabase(){
-    abstract fun alarmDao() : AlarmDao
-    abstract fun reminderDao() : ReminderDao
+//    abstract fun alarmDao() : AlarmDao
+//    abstract fun reminderDao() : ReminderDao
     abstract fun noteDao() : NoteDao
 }
