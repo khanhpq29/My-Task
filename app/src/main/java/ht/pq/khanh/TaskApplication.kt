b42bbd8523618540fe735e2e6f4f8c92bc36659c
Phan Khanh <khanhpq@Phongs-Mac-mini.local>
2017-11-10 13:38:52 +0700

    revert

--- app/src/main/java/ht/pq/khanh/TaskApplication.kt
+++ app/src/main/java/ht/pq/khanh/TaskApplication.kt
@@ -1,16 +1,20 @@
 package ht.pq.khanh
 
 import android.app.Application
+import android.arch.persistence.room.Room
 import android.content.Context
 import com.squareup.leakcanary.LeakCanary
 import com.squareup.leakcanary.RefWatcher
+import ht.pq.khanh.model.TaskDatabase
 
 /**
  * Created by khanhpq on 10/3/17.
  */
 class TaskApplication : Application() {
     private lateinit var refWatcher: RefWatcher
-
+    companion object {
+        lateinit var db : TaskDatabase
+    }
     fun getRefWatcher(context: Context): RefWatcher {
         val application = context.applicationContext as TaskApplication
         return application.refWatcher
@@ -19,5 +23,6 @@ class TaskApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         refWatcher = LeakCanary.install(this)
+        TaskApplication.db = Room.databaseBuilder(this, TaskDatabase::class.java, "note.db").build()
     }
 }
